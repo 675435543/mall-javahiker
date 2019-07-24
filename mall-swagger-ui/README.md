b1d4e9032f78a45f3abae38a3c4769976c845749
javahikers <472407594.qq.com>
2019-07-24 22:01:46 +0800

    'modify'

--- mall-swagger-ui/README.md
+++ mall-swagger-ui/README.md
@@ -1,8 +1,8 @@
 ## mall-swagger-ui 工程完全参考作者下面的教程搭建
 + [mall整合Swagger-UI实现在线API文档](https://juejin.im/post/5cf9035cf265da1bb47d54f8)
 
-注意点
 
+    注意点
     为当前包下controller生成API文档
     .apis(RequestHandlerSelectors.basePackage("com.macro.mall.tiny.controller"))
     为有@Api注解的Controller生成API文档
--- mall-swagger-ui/src/main/java/com/javahiker/mall/config/Swagger2Config.java
+++ mall-swagger-ui/src/main/java/com/javahiker/mall/config/Swagger2Config.java
@@ -26,9 +26,9 @@ public class Swagger2Config {
                 //为当前包下controller生成API文档
                 .apis(RequestHandlerSelectors.basePackage("com.javahiker.mall.controller"))
                 //为有@Api注解的Controller生成API文档
-                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+//                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
                 //为有@ApiOperation注解的方法生成API文档
-                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+//                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 .paths(PathSelectors.any())
                 .build();
     }
