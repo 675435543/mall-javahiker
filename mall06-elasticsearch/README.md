43a48d4963e7ae4991b0a9d3be492390f8458109
javahikers <472407594.qq.com>
2019-07-31 21:26:08 +0800

    'mall-elasticsearch'

---
+++ mall06-elasticsearch/README.md
@@ -0,0 +1,2 @@
+mall06-elasticsearch 工程完全参考作者下面的教程搭建
+- [mall整合Elasticsearch实现商品搜索](https://juejin.im/post/5cfba3e9f265da1b614fea60)
---
+++ mall06-elasticsearch/src/main/java/com/javahiker/mall/Mall06ElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.javahiker.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mall06ElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mall06ElasticsearchApplication.class, args);
+    }
+
+}
