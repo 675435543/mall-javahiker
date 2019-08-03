e4ac534421daa8ea6fc5966ac2471b30d9e2b36f
javahikers <472407594.qq.com>
2019-08-03 17:52:56 +0800

    mall-rabbitmmq

---
+++ mall08-rabbitmq/src/main/java/com/javahiker/mall/Mall08RabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.javahiker.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mall08RabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mall08RabbitmqApplication.class, args);
+    }
+
+}
